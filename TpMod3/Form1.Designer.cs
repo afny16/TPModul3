618ae7652018e7d559b1504679a7ec4c6dc7b27e
afny16 <121374489+afny16@users.noreply.github.com>
2023-03-05 20:39:55 +0700

    tambahan fitur GUI

--- TpMod3/Form1.Designer.cs
+++ TpMod3/Form1.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(609, 90);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.Text = "BUTTON";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(407, 238);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 25);
+            label1.TabIndex = 1;
+            label1.Text = "OUTPUT";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.ForeColor = SystemColors.InfoText;
+            textBox1.Location = new Point(34, 93);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(352, 31);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "INPUT";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            ForeColor = Color.Coral;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
     }
 }
--- TpMod3/Form1.cs
+++ TpMod3/Form1.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Emit;
+
 namespace TpMod3
 {
     public partial class Form1 : Form
@@ -5,6 +7,22 @@ namespace TpMod3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string button = textBox1.Text;
+            label1.Text = "Halooooo!!! " + button;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
